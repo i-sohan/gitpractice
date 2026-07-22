04ba1856884642428f042583d663b12c8c1ceecf
i-sohan <sohanmsis3@gmail.com>
2026-07-22 09:29:47 +0530

    dice

---
+++ github2/diceroller.py
@@ -0,0 +1,16 @@
+import random
+
+print("🎲 Welcome to the Dice Roller!")
+
+while True:
+    input("\nPress Enter to roll the dice...")
+
+    dice = random.randint(1, 6)
+
+    print(f"You rolled: {dice}")
+
+    choice = input("Roll again? (y/n): ").lower()
+
+    if choice != "y":
+        print("\nThanks for playing! 👋")
+        break
